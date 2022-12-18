d52eb4f2f2585551985da23105ebd5c205a0c8a0
ControlNet <smczx@hotmail.com>
2022-12-18 23:35:32 +1100

    fix dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,6 +11,13 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
         ],
     },
     resolve: {
