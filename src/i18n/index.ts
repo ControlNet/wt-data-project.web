d98f20d7071228f64e06f80fb2abedf6d4493c57
ControlNet <smczx@hotmail.com>
2022-12-19 20:41:59 +1100

    add en-us to i18n, add editorconfig

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,13 +2,15 @@ import { createI18n } from 'vue-i18n'
 
 
 import zh_cn from './lang/zh-cn.json'
+import en_us from './lang/en-us.json'
 
 
 const i18n = createI18n({
-    locale:'zh-cn',
-    messages:{
-        "zh-cn":zh_cn
+    locale: 'zh-cn',
+    messages: {
+        "zh-cn": zh_cn,
+        "en-us": en_us
     }
 })
 
-export {i18n}
+export { i18n }
