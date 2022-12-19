2a64fb2ad10e0d4b455018d41d632797addd305f
FeiBam <feibam19690104@gmail.com>
2022-12-19 14:54:12 +0900

    delete all d3 code, just use vue3 to refactor,add heatMap.vue

---
+++ src/components/heatMap.vue
@@ -0,0 +1,190 @@
+<script setup lang="ts">
+
+
+interface CountryData {
+    country:string,
+    win:number
+    br:string
+}
+
+interface TabelData{
+    country:string
+    detail:Array<CountryData>
+}
+
+
+
+const data:Array<TabelData> = [
+    {
+        country:'美国',
+        detail:[
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            },
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            },
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            },
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            },
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            }
+        ]
+    },
+    {
+        country:'美国',
+        detail:[
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            },
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            },
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            },
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            },
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            }
+        ]
+    },
+    {
+        country:'美国',
+        detail:[
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            },
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            },
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            },
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            },
+            {
+                country:'美国',
+                win:180,
+                br:'4.3-1.3'
+            }
+        ]
+    }
+]
+
+//todo:编写获取数据部分和 vue store交互部分
+
+
+</script>
+
+
+<template>
+    <div class="flex heatmap-contian">
+        <div class="flex" id="heat-mapx">
+            <div class="flex br-heatmap-y" id="br-heatmap-y">
+            </div>
+            <div id="heat-map">
+                <div class="flex heatmap-table">
+                    <div class="flex heatmap-table-col"
+                        v-for="item of data"
+                        :key="item.country"
+                    >
+                        <div class="heatmap-table-cell"
+                            v-for="cell of item.detail"
+                            :key="cell.win"
+                            ></div>
+                        <div class="heatmap-table-country">{{ item.country }}</div>
+                    </div>
+                </div>
+            </div>
+            <div id="color-bar">
+            </div>
+        </div>
+        <div id="heat-map-y">
+
+        </div>
+    </div>
+</template>
+
+<style>
+.flex{
+    display: flex;
+}
+
+.heatmap-contian{
+    display: flex;
+    height: auto;
+}
+.br-heatmap-y{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    margin-right: 20px;
+    min-width: 90px;
+}
+.br-heatmap-y > div {
+    height: 26px;
+}
+.heat-mapx{
+    display: flex;
+}
+
+.heatmap-table{
+    flex-flow: row;
+}
+
+.heatmap-table-col{
+    flex-flow: column;
+    align-items: center;
+}
+
+.heatmap-table-cell{
+    width: 48px;
+    height: 24px;
+    transition: 0.3s;
+    border: solid 1px;
+    background-color: aqua;
+}
+.heatmap-table-cell:hover{
+    transform: scale(1.3);
+}
+.heatmap-table-country{
+    margin-top: 14px;
+    font-size: 13px;
+}
+</style>
--- src/views/heatMapView.vue
+++ src/views/heatMapView.vue
@@ -1,4 +1,23 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { d3DataStore } from "@/stores/store" 
+
+import type { MetaData } from "@/stores/store"
+import type { Ref } from 'vue'
+
+
+import heatMap from "@/components/heatMap.vue"
+
+
+
+const metaData: Ref<MetaData[] | null> = ref(null)
+const selectDate :Ref<string> = ref('')
+const selectBr :Ref<Number> = ref(0)
+
+d3DataStore.dispatch('getMetaData').then((data)=>{
+    metaData.value = d3DataStore.getters.getJoinedMetaData
+})
+
 
 </script>
 
@@ -8,7 +27,16 @@
         <el-aside>
             <el-row :justify="'space-between'" :align="'middle'">
                 <el-col :span="4"><h1>日期:</h1></el-col>
-                <el-col :span="18"><el-select></el-select></el-col>
+                <el-col :span="18">
+                    <el-select v-model="selectDate">
+                        <el-option 
+                        v-for="item in metaData"
+                        :key="item.date"
+                        :label="item.date"
+                        :value="item.date"
+                        />
+                    </el-select>
+                </el-col>
             </el-row>
             <el-row :justify="'space-between'" :align="'middle'">
                 <el-col :span="6"><h1>指标:</h1></el-col>
@@ -16,7 +44,12 @@
             </el-row>
             <el-row :justify="'space-between'" :align="'middle'">
                 <el-col :span="6"><h1>载具类型:</h1></el-col>
-                <el-col :span="18"><el-select></el-select></el-col>
+                <el-col :span="18">
+                    <el-select>
+                        <el-option :value="'地面载具'"/>
+                        <el-option :value="'空中载具'"/>
+                    </el-select>
+                </el-col>
             </el-row>
             <el-row :justify="'space-between'" :align="'middle'">
                 <el-col :span="6"><h1>游戏模式:</h1></el-col>
@@ -24,11 +57,18 @@
             </el-row>
             <el-row :justify="'space-between'" :align="'middle'">
                 <el-col :span="6"><h1>分房区间:</h1></el-col>
-                <el-col :span="18"><el-select></el-select></el-col>
+                <el-col :span="18">
+                    <el-select v-model="selectBr">
+                        <el-option
+                        />
+                    </el-select>
+                </el-col>
             </el-row>
         </el-aside>
         <el-divider style="height: auto;" direction="vertical" />
-        <el-main style="height:1000px">1</el-main>
+        <el-main id="d3enterPoint" >
+            <heatMap></heatMap>
+        </el-main>
     </el-container>
 </template>
 
