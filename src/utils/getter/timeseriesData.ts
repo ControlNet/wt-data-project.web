993eaaf92a5ee6d40186296828ad04a38bb96ed5
n0099 <n@n0099.net>
2022-12-22 03:53:22 +0800

    Update timeseriesData.ts

--- src/utils/getter/timeseriesData.ts
+++ src/utils/getter/timeseriesData.ts
@@ -2,6 +2,14 @@ import type { TimeseriesABRow, TimeseriesRBRow, TimeseriesRow, TimeseriesSBRow }
 import { Getter } from "@/utils/getter/getter";
 import type { Measurement, Mode } from "@/types/options";
 
+type TimeseriesRow<T> = T extends 'ab'
+    ? TimeseriesABRow
+    : T extends 'rb'
+        ? TimeseriesRBRow
+        : T extends 'sb'
+            ? TimeseriesSBRow
+            : never;
+
 export class TimeseriesRowGetter<T extends TimeseriesRow = TimeseriesRow> extends Getter {
     protected readonly data: T;
     protected readonly mode: Mode;
@@ -15,59 +23,14 @@ export class TimeseriesRowGetter<T extends TimeseriesRow = TimeseriesRow> extend
     }
 
     get value(): number {
-        switch (this.mode) {
-            case "ab": {
-                const abData = <TimeseriesABRow><unknown>this.data;
-                switch (this.measurement) {
-                    case "win_rate":
-                        return +abData.ab_win_rate;
-                    case "battles_sum":
-                        return +abData.ab_battles_sum;
-                }
-                break;
-            }
-            case "rb": {
-                const rbData = <TimeseriesRBRow><unknown>this.data;
-                switch (this.measurement) {
-                    case "win_rate":
-                        return +rbData.rb_win_rate;
-                    case "battles_sum":
-                        return +rbData.rb_battles_sum;
-                }
-                break;
-            }
-            case "sb": {
-                const sbData = <TimeseriesSBRow><unknown>this.data;
-                switch (this.measurement) {
-                    case "win_rate":
-                        return +sbData.sb_win_rate;
-                    case "battles_sum":
-                        return +sbData.sb_battles_sum;
-                }
-                break;
-            }
-        }
+        return +this.data[`${this.mode}_${this.measurement}`] as unknown as TimeseriesRow<typeof this.mode>;
     }
 
     get br(): string {
-        switch (this.mode) {
-            case "ab":
-                return (<TimeseriesABRow><unknown>this.data).ab_br;
-            case "rb":
-                return (<TimeseriesRBRow><unknown>this.data).rb_br;
-            case "sb":
-                return (<TimeseriesSBRow><unknown>this.data).sb_br;
-        }
+        return this.data[`${this.mode}_br`] as unknown as TimeseriesRow<typeof this.mode>;
     }
 
     get lowerBr(): number {
-        switch (this.mode) {
-            case "ab":
-                return +(<TimeseriesABRow><unknown>this.data).ab_lower_br;
-            case "rb":
-                return +(<TimeseriesRBRow><unknown>this.data).rb_lower_br;
-            case "sb":
-                return +(<TimeseriesSBRow><unknown>this.data).sb_lower_br;
-        }
+        return +this.data[`${this.mode}_lower_br`] as unknown as TimeseriesRow<typeof this.mode>;
     }
 }
